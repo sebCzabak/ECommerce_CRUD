fe4ce2f1b7e27acb106a419bb4dbeb49dc66dd5c
Sebastian <sczabak@gmail.com>
2022-03-14 12:33:25 +0100

    Initial commit

---
+++ H2-CRUD-For-ECommerce/src/test/java/sebCzabak/H2/CRUD/For/ECommerce/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sebCzabak.H2.CRUD.For.ECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
